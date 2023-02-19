87cd9944fed30040285fc09c0af7f85dbb984116
henriquecfaguiar <henriquecfaguiar@gmail.com>
2023-02-19 16:50:39 -0300

    Footer minor adjustments and update favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,6 @@ module.exports = {
   content: ['./*.html'],
   theme: {
     extend: {
-      colors: {
-        'footer': '#454158',
-      },
       fontFamily: {
         'sans': ['Poppins', 'sans-serif'],
         'JetBrainsMono': ['JetBrains Mono', 'sans-serif']
